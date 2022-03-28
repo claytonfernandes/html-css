793f45932cf5df64129599ebba803870373f199d
Clayton Fernandes <98171119+claytonfernandes@users.noreply.github.com>
2022-03-28 14:41:40 -0300

    Atualização aula 12

--- JS/aula12ex/ex014/script.js
+++ JS/aula12ex/ex014/script.js
@@ -1,1 +1,18 @@
-alert("Olá, mundo!")
+function carregar(){
+var msg = window.document.getElementById('msg')
+var img = window.document.getElementById('imagem')
+var data = new Date()
+var hora = data.getHours()
+//var hora = 8
+msg.innerHTML = `Agora são ${hora} horas.`
+if(hora >= 0 && hora < 12){
+    //Bom Dia!
+        img.src = 'foto-manha.png'
+    } else if (hora >= 12 && hora <= 18){
+        //Boa tarde!
+        img.src = 'foto-tarde.png'
+    } else {
+        //Boa Noite!
+        img.src = 'foto-noite.png'
+    }
+}
