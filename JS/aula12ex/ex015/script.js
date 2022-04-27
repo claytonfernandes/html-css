0eeddfde90beee761b126a124b103682a9372a86
Clayton Fernandes <98171119+claytonfernandes@users.noreply.github.com>
2022-04-27 19:19:58 -0300

    Aula 12Ex exercício 15

---
+++ JS/aula12ex/ex015/script.js
@@ -0,0 +1,8 @@
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.querySelector('div#res')
+    if (fano.value.length == 0 || fano.value > ano)
+    window.alert('[ERRO] Verifique se os dados estão corretos!')
+}
