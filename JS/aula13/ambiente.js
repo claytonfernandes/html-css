32ca21cf3b47e8e0060b334379237d0d8e668495
Clayton Fernandes <98171119+claytonfernandes@users.noreply.github.com>
2022-04-30 15:27:06 -0300

    Aula 13 / Aula 14

---
+++ JS/aula13/ambiente.js
@@ -0,0 +1,14 @@
+/*
+Testa e executa depois
+var c = 1
+while (c <= 500) {
+    console.log(`passo ${c}`)
+    c++
+}
+*/
+//Executa e testa depois
+var c = 1
+do {
+    console.log(`passo ${c}`)
+    c++
+} while (c <= 500)
