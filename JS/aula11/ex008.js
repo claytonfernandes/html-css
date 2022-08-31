b1fd158615a5e01b146e4afe24755f9408b86943
Clayton Fernandes <98171119+claytonfernandes@users.noreply.github.com>
2022-08-31 14:36:10 -0300

    Atualização

--- JS/aula11/ex008.js
+++
@@ -1,6 +0,0 @@
-var vel = 60.1
-console.log(` A velocodade de seu carro é ${vel}Km/h.`)
-if (vel > 60) { //condição simples
-    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
-}
-console.log(`Dirija sempre usando o cinto de segurança!`)
